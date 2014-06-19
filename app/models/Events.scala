0e8d66293136f2d3baf5534b6ea40737a83ace07
yugolf <yugo.maede@gmail.com>
2014-06-19 15:15:37 +0900

    イベント検索一覧機能を作成

--- app/models/Events.scala
+++ app/models/Events.scala
@@ -2,6 +2,7 @@ package models
 
 import play.api.db.DB
 import play.api.Play.current
+import play.Logger
 import scala.slick.driver.H2Driver.simple._
 import models.Tables.{ Event, EventRow }
 import scala.slick.jdbc.meta.{ MTable, createModel }
@@ -16,6 +17,18 @@ object Events {
   def create(e: EventRow) = database.withTransaction { implicit session: Session =>
     Event.insert(e)
   }
+
+  /** 検索 */
+  def find(eventId: String, eventNm: String): List[EventRow] =
+    database.withTransaction { implicit session: Session =>
+
+      var q = if (eventId.isEmpty) Event else Event.filter(_.eventId === eventId)
+      q = if (eventNm.isEmpty) q else q.filter(_.eventNm like s"%$eventNm%")
+      q = q.sortBy(_.eventNm)
+      Logger.debug(q.selectStatement)
+
+      return q.list
+    }
 
   def model = database.withSession { implicit session =>
     val tables = MTable.getTables(None, Some("TECHAPP"), None, None).list
